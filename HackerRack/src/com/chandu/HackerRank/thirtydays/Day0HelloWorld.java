f4f3a1ed60cad590d6edf33334f9b28c2f57378f
Chandu Reddy <70282603+chandusreddy@users.noreply.github.com>
2021-02-15 09:44:00 -0800

    Add files via upload
    Initial version - Hacker Rank Solution

---
+++ HackerRack/src/com/chandu/HackerRank/thirtydays/Day0HelloWorld.java
@@ -0,0 +1,16 @@
+package com.chandu.HackerRank.thirtydays;
+
+import java.util.Scanner;
+
+public class Day0HelloWorld {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String InputString = scan.nextLine();
+		scan.close();
+
+		System.out.println("Hello World");
+		System.out.println(InputString.toString());
+	}
+
+}
